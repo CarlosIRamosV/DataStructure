2cd93746b163e65a7d0cf309c94284d09b33704b
CarlosIRamosV <57056947+CarlosIRamosV@users.noreply.github.com>
2022-10-23 00:21:03 -0600

    Deprecated

--- TDAVector/src/main/java/me/CarlosIRamosV/TDAVector/interfaces/TDAVectorInterfaceOld.java
+++ TDAVector/src/main/java/me/CarlosIRamosV/TDAVector/interfaces/TDAVectorInterfaceOld.java
@@ -1,5 +1,13 @@
 package me.CarlosIRamosV.TDAVector.interfaces;
 
+/**
+ * Interfaz que define los métodos que debe implementar el TDA Vector
+ *
+ * @author Carlos Ramos
+ * @version 1.0
+ * @deprecated Esta interfaz no se debe usar, se recomienda usar la interfaz {@link TDAVectorInterface}
+ */
+@Deprecated
 public interface TDAVectorInterfaceOld {
     boolean agregarElementoFinal(int dato);
 
